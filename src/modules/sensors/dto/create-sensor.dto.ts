8a5fb28bfdadf467a4038ce91b059d1114da70bd
Ilde'R <ilderios0304@gmail.com>
2026-06-11 09:37:59 -0600

    feat: Agregar validacion psi

--- src/modules/sensors/dto/create-sensor.dto.ts
+++ src/modules/sensors/dto/create-sensor.dto.ts
@@ -1,4 +1,10 @@
-import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  Min,
+} from 'class-validator';
 
 export class CreateSensorDto {
   @IsOptional()
@@ -14,5 +20,11 @@ export class CreateSensorDto {
 
   @IsNotEmpty({ message: 'La lectura de PSI es obligatoria' })
   @IsNumber({}, { message: 'El valor de PSI debe ser un número válido' })
+  @Min(0, { message: 'La presión (PSI) no puede ser un número negativo' })
   psi!: number;
+
+  @IsOptional()
+  @IsNumber({}, { message: 'El umbral debe ser un número' })
+  @Min(0, { message: 'El umbral no puede ser un número negativo' })
+  currentThreshold?: number;
 }
