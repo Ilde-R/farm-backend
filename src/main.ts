0615052629d24fbce655f5842d83f67b5e60d21c
Ilde-R <ilderios0304@gmail.com>
2026-09-06 02:51:25 -0600

    fix: Helmet para scalar

--- src/main.ts
+++ src/main.ts
@@ -23,15 +23,22 @@ async function bootstrap() {
       contentSecurityPolicy: {
         directives: {
           defaultSrc: ["'self'"],
-          scriptSrc: ["'self'", "'unsafe-inline'", 'https://cdn.jsdelivr.net'],
+          scriptSrc: [
+          "'self'",
+          "'unsafe-inline'",
+          'https://cdn.jsdelivr.net',
+          'https://cdn.scalar.com',
+        ],
           styleSrc: [
             "'self'",
             "'unsafe-inline'",
             'https://cdn.jsdelivr.net',
             'https://fonts.googleapis.com',
+            'https://cdn.scalar.com',
           ],
           fontSrc: ["'self'", 'https://fonts.gstatic.com'],
           imgSrc: ["'self'", 'data:', 'https:'],
+          connectSrc: ["'self'", 'http://78.13.219.157:3000'],
         },
       },
     }),
