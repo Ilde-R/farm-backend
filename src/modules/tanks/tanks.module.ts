d8a98fc681fb5af4754009923aa20091b9e48f7d
Ilde-R <ilderios0304@gmail.com>
2026-09-13 00:03:46 -0600

    feat: Agregar repository a modulo de tanques

--- src/modules/tanks/tanks.module.ts
+++ src/modules/tanks/tanks.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TanksService } from './tanks.service';
 import { TanksController } from './tanks.controller';
+import { TankRepository } from './repositories/tank.repository';
 
 @Module({
   controllers: [TanksController],
-  providers: [TanksService],
+  providers: [TanksService, TankRepository],
 })
 export class TanksModule {}
