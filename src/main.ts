d55e2170024ba4c5a2860cf8381225c4ba2d4e0a
Ilde-R <ilderios0304@gmail.com>
2026-09-13 16:21:02 -0600

    fix: Desabilitar CORS

--- src/main.ts
+++ src/main.ts
@@ -25,22 +25,12 @@ async function bootstrap() {
       contentSecurityPolicy: {
         directives: {
           defaultSrc: ["'self'"],
-          scriptSrc: [
-            "'self'",
-            "'unsafe-inline'",
-            'https://cdn.jsdelivr.net',
-            'https://cdn.scalar.com',
-          ],
-          styleSrc: [
-            "'self'",
-            "'unsafe-inline'",
-            'https://cdn.jsdelivr.net',
-            'https://fonts.googleapis.com',
-            'https://cdn.scalar.com',
-          ],
+          scriptSrc: ["'self'", "'unsafe-inline'", 'https://cdn.jsdelivr.net', 'https://cdn.scalar.com'],
+          styleSrc: ["'self'", "'unsafe-inline'", 'https://cdn.jsdelivr.net', 'https://fonts.googleapis.com', 'https://cdn.scalar.com'],
           fontSrc: ["'self'", 'https://fonts.gstatic.com'],
           imgSrc: ["'self'", 'data:', 'https:'],
-          connectSrc: ["'self'", 'http://78.13.219.157:3000'], // Permitido tu entorno dev HTTP
+          connectSrc: ["'self'", 'http://78.13.219.157:3000'], 
+          upgradeInsecureRequests: null,
         },
       },
     })
